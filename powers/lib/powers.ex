2e95fd1bc2b9c08a220dda90972d16e2017f428d
jbratton <jeremy@alumni.rice.edu>
2016-03-24 22:55:33 -0500

    implement gross nth_root function

--- powers/lib/powers.ex
+++ powers/lib/powers.ex
@@ -15,4 +15,25 @@ defmodule Powers do
 
   def raise(_, 0, accumulator), do: accumulator
   def raise(x, n, accumulator), do: Powers.raise(x, n - 1, x * accumulator)
+
+  @doc """
+  A function to calculate the nth root of an integer using the Newton-Raphson method.
+  """
+  def nth_root(x, n), do: nth_root(x, n, x / 2.0)
+  def nth_root(x, n, approximation) do
+    IO.puts("Current guess is #{approximation}")
+    delta = 1.0e-8
+
+    f = Powers.raise(approximation, n) - x
+    f_prime = n * Powers.raise(approximation, n - 1)
+
+    next_approximation = approximation - f / f_prime
+
+    change = abs(next_approximation - approximation)
+    if change < delta do
+      next_approximation
+    else
+      nth_root(x, n, next_approximation)
+    end
+  end
 end
--- powers/test/powers_test.exs
+++ powers/test/powers_test.exs
@@ -2,19 +2,23 @@ defmodule PowersTest do
   use ExUnit.Case
   doctest Powers
 
-  test "any number to the power 0 is 1" do
+  test "raise: any number to the power 0 is 1" do
     assert Powers.raise(73, 0) == 1
   end
 
-  test "any number to the power 1 is itself" do
+  test "raise: any number to the power 1 is itself" do
     assert Powers.raise(342, 1) == 342
   end
 
-  test "for positive n, x^n is x * x^(n-1)" do
+  test "raise: for positive n, x^n is x * x^(n-1)" do
     assert Powers.raise(3, 4) == 3 * Powers.raise(3, 3)
   end
 
-  test "for negative n, x^n is 1.0/x^(-n)" do
+  test "raise: for negative n, x^n is 1.0/x^(-n)" do
     assert Powers.raise(3, -2) == 1.0 / Powers.raise(3, 2)
+  end
+
+  test "nth_root" do
+    assert Powers.nth_root(36, 2) == 6
   end
 end
